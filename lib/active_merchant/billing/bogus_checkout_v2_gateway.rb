e60a0bd7db889457a3bfe42d39e90b70cd747ebc
Jeff Yucis <jyucis@workarea.com>
2019-11-19 16:37:17 -0500

    Move credit card tokenization to post transaction
    Checkout.com charges for every authorization transaction. Moving tokenzation to post
    transaction helps reduce the extra cost for the merchant.
    
    CHECKOUTDOTCOM-1

--- lib/active_merchant/billing/bogus_checkout_v2_gateway.rb
+++ lib/active_merchant/billing/bogus_checkout_v2_gateway.rb
@@ -13,6 +13,30 @@ module ActiveMerchant
           raise Error, error_message(paysource)
         end
       end
+
+      def authorize_swipe(money, paysource, options = {})
+        money = amount(money)
+        case normalize(paysource)
+        when /1$/, AUTHORIZATION
+          Response.new(true, SUCCESS_MESSAGE, { "source" => { "id" => "111", "customerId" => "cust_123" }, "authorized_amount" => money }, test: true, authorization: AUTHORIZATION)
+        when /2$/
+          Response.new(false, FAILURE_MESSAGE, { authorized_amount: money, error: FAILURE_MESSAGE }, test: true, error_code: STANDARD_ERROR_CODE[:processing_error])
+        else
+          raise Error, error_message(paysource)
+        end
+      end
+
+      def purchase_swipe(money, paysource, options = {})
+        money = amount(money)
+        case normalize(paysource)
+        when /1$/, AUTHORIZATION
+          Response.new(true, SUCCESS_MESSAGE, { "source" => { "id" => "111", "customerId" => "cust_123" }, "paid_amount" => money }, test: true, authorization: AUTHORIZATION)
+        when /2$/
+          Response.new(false, FAILURE_MESSAGE, { paid_amount: money, error: FAILURE_MESSAGE }, test: true, error_code: STANDARD_ERROR_CODE[:processing_error])
+        else
+          raise Error, error_message(paysource)
+        end
+      end
     end
   end
 end
